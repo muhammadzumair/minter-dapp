ea71064dcdda40767b0376573815af90cfa36cdd
muhammadzumair <zumairmuhammad@gmail.com>
2022-03-26 13:10:45 +0500

    add contract address

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x0Ee72b47387e87B769a16241F127035Accb9a438"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
